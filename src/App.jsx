3e5f875069d4bd94109b07343210b1c005e04cd8
jrsilvadantas-coder <jr.silva.dantas@gmail.com>
2026-05-08 14:03:09 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 export default function App() {
   return (
-    <div style={{ padding: 40, fontFamily: 'Arial' }}>
-      <h1>Raíssa & José Roberto</h1>
-      <p>Nosso site está funcionando!</p>
+    <div>
+      <h1>Site funcionando</h1>
     </div>
   )
 }
